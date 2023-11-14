a59786cf5d6857bd1bde982ef289e990a827fc2a
ccortes <cicc81@gmail.com>
2023-11-13 21:01:34 -0600

    Commit para regular el estatus

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.ham-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const carritoProduct = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoProduct);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
@@ -13,5 +16,9 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoProduct() {
+    carritoProduct.classList.toggle('inactive');
 }
 
